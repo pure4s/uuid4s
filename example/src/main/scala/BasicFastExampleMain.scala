a3ff4a54bffac26d58f9ea93ce7a989656a28243
Cristina Delgado <crdelgro@gmail.com>
2019-02-04 12:09:16 +0100

    Remove what is used in example module

--- example/src/main/scala/BasicFastExampleMain.scala
+++ example/src/main/scala/BasicFastExampleMain.scala
@@ -1,30 +1,15 @@
 import java.util.UUID
 import cats.effect.IO
-import cats.implicits._
 import org.pure4s.uuid4s.FFUUID
-import org.pure4s.uuid4s.implicits._
 
 object BasicFastExampleMain extends App {
 
-  //Parsing
+  // Parsing
   val uuid1: UUID =
-    FFUUID[IO].fromString("7cfb70a9-0764-4851-a28c-309393aea2eb").unsafeRunSync()
+    FFUUID[IO]
+      .fromString("7cfb70a9-0764-4851-a28c-309393aea2eb")
+      .unsafeRunSync()
 
-  //Generating
+  // Generating
   val uuid2: UUID = FFUUID[IO].random.unsafeRunSync()
-
-  //Comparing
-  val result1: Boolean = uuid2 < uuid1
-  val result2: Boolean = uuid2 <= uuid1
-  val result3: Boolean = uuid1 > uuid2
-  val result4: Boolean = uuid1 >= uuid2
-  val result5: Boolean = uuid1 === uuid1
-
-  println(s" -> uuid1 = $uuid1")
-  println(s" -> uuid2 = $uuid2")
-  println(s" --> uuid2  <  uuid1 = $result1")
-  println(s" --> uuid2 <=  uuid1 = $result2")
-  println(s" --> uuid1  >  uuid2 = $result4")
-  println(s" --> uuid1 >=  uuid2 = $result4")
-  println(s" --> uuid1 === uuid1 = $result5")
 }
